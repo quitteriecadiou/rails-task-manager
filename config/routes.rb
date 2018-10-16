ba02b615ecd517815fa97883e0d5efa7920f310a
Quitterie Cadiou <quitterie.cadiou@gmail.com>
2018-10-16 14:21:17 +0200

    create task done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get '/tasks', to: 'tasks#index', as: 'tasks'
+  get 'tasks', to: 'tasks#index', as: 'tasks'
 
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  post 'tasks', to: 'tasks#create'
+
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
 end
