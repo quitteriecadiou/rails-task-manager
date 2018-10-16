4c8b4f582e322c2ca48f2952b3ea37bc54c3d49f
Quitterie Cadiou <quitterie.cadiou@gmail.com>
2018-10-16 15:11:56 +0200

    update and delete done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,21 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task.id)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+  end
+
   private
 
   def task_params
