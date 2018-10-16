67ad10b29b3892e12d9d66941a489434dd0500f0
Quitterie Cadiou <quitterie.cadiou@gmail.com>
2018-10-16 15:17:09 +0200

    update works

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,11 +30,12 @@ class TasksController < ApplicationController
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
+    redirect_to tasks_path
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
